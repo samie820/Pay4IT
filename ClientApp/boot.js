92a6354df4a9249497ac3a88a801194a339fda54
samuel <samuelomole70@gmail.com>
2018-09-03 23:39:48 +0100

    Initial commit

---
+++ ClientApp/boot.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+const routes = [
+];
+
+new Vue({
+    el: '#app-root',
+    router: new VueRouter({ mode: 'history', routes: routes }),
+    render: h => h(require('./components/App.vue'))
+});
---
+++ ClientApp/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+<h1>Welcome to Hands on Vue.js with ASP.NET Core!</h1>
+<p>
+The time is: {{ time }}
+</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      time: new Date().toString()
+    };
+  }
+};
+</script>
